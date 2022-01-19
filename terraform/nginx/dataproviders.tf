894b0520f06a40eceadeb39913a7ffa5de7208b7
Martin Proffitt <mproffitt@choclab.net>
2022-01-19 16:45:27 +0100

    Remove hardcoded values from external address script

--- terraform/nginx/dataproviders.tf
+++ terraform/nginx/dataproviders.tf
@@ -5,21 +5,23 @@ data "external" "addresses" {
     <<EOT
 (
   echo '{';
-  for node in node1 node2 node3; do
+  for node in $(vboxmanage list vms | grep '${var.domain}' | awk -F. '{ gsub("\"", ""); print $1; }'); do
     echo -n "$node ";
-    vboxmanage guestproperty enumerate $node.choclab.net | grep '.*IP.*'$(
+    vboxmanage guestproperty enumerate $node.${var.domain} | grep '.*IP.*'$(
       ip --json a show dev enp5s0 | jq -r '.[0].addr_info[] | select(.family=="inet").local' | cut -d. -f1-3
-    ) | awk '{ gsub(",", ""); print "\""$4"\""}';
-  done | awk 'BEGIN {l=0}{
+    ) | awk '{
+      gsub(",", "");
+      print "\""$4"\""
+    }';
+  done | awk 'BEGIN { l=0 } {
     array[l++]=(sprintf("\"%s\": %s", $1, $2));
-  }
-  END {
+  } END {
     for (i=0; i<l; i++) {
       printf("%s", array[i]);
       if (i<l-1) print ","
     }
   }';
- echo '}'
+  echo '}'
 ) | jq
 EOT
   ]
